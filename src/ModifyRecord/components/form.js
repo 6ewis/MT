755e5c6e0b395925ed537cfb7ece24e6ce517b02
Lewis Dackam <lewis.dackam@codancanada.com>
2016-05-04 19:56:33 -0400

    extracted spinner in its own component and fixed Registered address

--- src/ModifyRecord/components/form.js
+++ src/ModifyRecord/components/form.js
@@ -14,29 +14,7 @@ import DateInput from './dateInput';
 import Countries from './countries.js';
 
 import R from 'ramda';
-import Spinner from 'react-spinjs';
-//extract the spinner in its own component
-const opts = {
-  lines: 11 // The number of lines to draw
-, length: 26 // The length of each line
-, width: 27 // The line thickness
-, radius: 42 // The radius of the inner circle
-, scale: 1 // Scales overall size of the spinner
-, corners: 1 // Corner roundness (0..1)
-, color: '#000' // #rgb or #rrggbb or array of colors
-, opacity: 0.25 // Opacity of the lines
-, rotate: 0 // The rotation offset
-, direction: 1 // 1: clockwise, -1: counterclockwise
-, speed: 1 // Rounds per second
-, trail: 60 // Afterglow percentage
-, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-, zIndex: 2e9 // The z-index (defaults to 2000000000)
-, className: 'spinner' // The CSS class to assign to the spinner
-, top: '50vh' // Top position relative to parent
-, left: '50%' // Left position relative to parent
-, shadow: false // Whether to render a shadow
-, hwaccel: false // Whether to use hardware acceleration
-};
+import _Spinner from './shared/_Spinner.js';
 
 export default (
   {aliases, entity_type, countries, billing_clients,
@@ -45,7 +23,7 @@ export default (
    deserialized_concatenated_registered_address,
    nationality, residence, domicile, telex}) => {
 
-   if (R.isEmpty(billing_clients)) {return <Spinner config={opts}/>; }
+   if (R.isEmpty(billing_clients)) {return <_Spinner/>; }
 
            //this is for addresses - will be extracted in its own component- WIP
            const onlyTheRegisteredAddresses = R.filter( R.propEq('address_type', 'Registered'), matter_specific_addresses);
@@ -54,10 +32,10 @@ export default (
               ${item.positions}`;
            const options = R.map(returnEntitySpecificOption, onlyTheRegisteredAddresses);
 
-           //utility func
+ //utility func
  const maybe = (method) => {
-   if (deserialized_concatenated_registered_address === true) {
-     return (deserialized_concatenated_registered_address.registeredAddressFields === true) ?
+   if (!R.isNil(deserialized_concatenated_registered_address)) {
+     return !R.isNil(deserialized_concatenated_registered_address.registeredAddressFields) ?
        deserialized_concatenated_registered_address.registeredAddressFields[method] :
        null;
    }
