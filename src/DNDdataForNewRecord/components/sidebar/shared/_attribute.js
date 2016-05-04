3e7e6b2be9d0172251e81e4bd0c1e662e835fd97
Lewis Dackam <lewis.dackam@codancanada.com>
2016-05-04 12:43:16 -0400

    fixed the way the addresses are displayed on the sidebar

--- src/DNDdataForNewRecord/components/sidebar/shared/_attribute.js
+++ src/DNDdataForNewRecord/components/sidebar/shared/_attribute.js
@@ -3,15 +3,31 @@ import R from 'ramda';
 
 export default ({attributeValue, id, connectDragSource,
                  opacity, backgroundColor, cursor}) => {
-  function renderAttributeValue() {
+
+
+  const removeNullAndBlankStringsInArray = (array) => {
+    const isAString = R.is(String);
+    const trimString = R.when(isAString, R.trim);
+    const isAnEmptyString = (text) => R.isEmpty(trimString(text));
+    const isANullValue = (text) => R.isNil(text);
+    const nullOrEmpty = R.either(isANullValue, isAnEmptyString);
+
+    return R.reject(nullOrEmpty, array);
+  };
+
+  const renderAttributeValue = () => {
     //take into account new line character
-    return attributeValue.split('\n')
-      .map((item, index) => <span key={index}>{item}<br/></span>);
-  }
+    const splitAttribute = removeNullAndBlankStringsInArray(attributeValue.split('\n'));
+    const splitAttributeLength = splitAttribute.length;
+console.log(JSON.stringify(splitAttribute));
+    return splitAttribute
+      .map((item, index) =>
+         <div key={index}><span>{item}</span><br/>{((index > 0) && (index === (splitAttributeLength - 1))) ? <br/> : null}</div>);
+  }; //it could returns an empty array
 
   return connectDragSource(
     <div className='DNDhover' style={{opacity, backgroundColor, cursor}}>
-      {!R.isNil(attributeValue) ? renderAttributeValue() : ''}
+      {R.isNil(attributeValue) || R.isEmpty(renderAttributeValue()) ? null : renderAttributeValue()}
     </div>
   );
 };
